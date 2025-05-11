5ea38bb264c6fee19c69d9232595b6761d2ac2e9
MachineWorks <30751311+vsrin@users.noreply.github.com>
2025-05-11 07:50:51 -0400

    Add files via upload
    Removed all comments

--- pii_redactor_tool.py
+++ pii_redactor_tool.py
@@ -3,7 +3,6 @@
 import os
 import re
 from typing import Dict, Any, List, Optional
-#from tool_py_base_class import BaseTool
 from Blueprint.Templates.Tools.python_base_tool import BaseTool
 
 class PIIRedactorTool(BaseTool):
